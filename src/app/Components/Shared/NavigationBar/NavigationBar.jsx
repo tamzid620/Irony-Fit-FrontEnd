bcd1d92c5e7b09a0a67db0bc230da6f9b6f63997
tamzid620 <tamzid620@gmail.com>
2025-02-10 18:14:25 +0600

    navbar font added and small device responsive addced

--- src/app/Components/Shared/NavigationBar/NavigationBar.jsx
+++ src/app/Components/Shared/NavigationBar/NavigationBar.jsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
 import React from "react";
-import navImage from "@/assests/icons/NavImage.png";
-import arrowImage from "@/assests/icons/right-chevron.png";
-import Link from "next/link";
-import { Nunito } from "next/font/google";
 import "./NavigationBar.css";
+import Link from "next/link";
+import Image from "next/image";
+import navImage from "@/assests/icons/NavImage.png";
+import menuImage from "@/assests/icons/menubar.png";
+import arrowImage from "@/assests/icons/right-chevron.png";
+import { Nunito } from "next/font/google";
+import { TbArrowBadgeRightFilled } from "react-icons/tb";
 
 const nunito = Nunito({
   weight: ["300", "400", "700", "900"],
@@ -32,7 +34,7 @@ const NavigationBar = () => {
   ];
 
   return (
-    <nav className={`${nunito.className} `}>
+    <nav className={`${nunito.className} bg-white text-black`}>
       {/* navlist for large and medium device */}
       <div className="lg:flex md:flex sm: hidden justify-between items-center  mx-6 ">
         {/* image section  */}
@@ -59,10 +61,39 @@ const NavigationBar = () => {
           </h1>
         </button>
       </div>
-       {/* navlist for small device */}
-       <div className="md:hidden sm: block  mx-6 ">
-
-       </div>
+      {/* navlist for small device */}
+      <div className="md:hidden sm: block">
+        <div className="flex justify-between items-center mx-6 py-2">
+          {/* image section  */}
+          <div>
+            <Image src={navImage} alt="navbar image" className="w-[50px]" />
+          </div>
+          {/* menubar section  */}
+          <div>
+            <Image src={menuImage} alt="navbar image" className="w-[30px] drop-shadow-lg" />
+          </div>
+        </div>
+         {/* join button  */}
+         <div className="flex justify-center bg-[#1AAD49] group">
+         <button className="uppercase font-extrabold text-sm flex items-center py-4 text-white group-hover:text-black"> 
+         <TbArrowBadgeRightFilled size={25}  />
+          <h1 className="relative group">
+            <span className="relative text-lg">Join</span>
+          </h1>
+        </button>
+         </div>
+         {/* ul section  */}
+        <ul className="block text-center ">
+          {navlist.map(({ path, name }, index) => (
+            <Link key={index} href={path} className="relative group">
+              <li className=" uppercase font-extrabold text-sm relative leading-[50px]">
+                {name}
+              </li>
+              <hr className="navHrPolygen absolute top-5 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
+            </Link>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
