4b02618b276372960c875fd14b216ffa0e8ff69d
tamzid620 <tamzid620@gmail.com>
2025-03-04 11:57:50 +0600

    subset added in testimonial component

--- src/app/Components/Shared/UserShared/Testimonial/Testimonial.jsx
+++ src/app/Components/Shared/UserShared/Testimonial/Testimonial.jsx
@@ -8,13 +8,13 @@ import Image from "next/image";
 
 const rubik = Rubik({
   weight: ["300", "400", "700", "900"],
-  //   subsets: ["latin"],
+    subsets: ["latin"],
 preload: true, 
 });
 
 const nunito = Nunito({
   weight: ["300", "400", "700", "900"],
-  //   subsets: ["latin"],
+    subsets: ["latin"],
 preload: true, 
 });
 
