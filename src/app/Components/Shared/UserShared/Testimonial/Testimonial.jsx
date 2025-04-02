05a8f1d9c81b3bd6ae16e697053423993a86a1c2
tamzid620 <tamzid620@gmail.com>
2025-04-02 20:04:35 +0600

    testimonial compo information added

--- src/app/Components/Shared/UserShared/Testimonial/Testimonial.jsx
+++ src/app/Components/Shared/UserShared/Testimonial/Testimonial.jsx
@@ -1,22 +1,31 @@
 import React from "react";
-import { Rubik, Nunito } from "next/font/google";
 import coverPhoto from "@/assests/images/SecondCover.jpg";
 import arrowPhoto from "@/assests/icons/right-chevron.png";
 import apostrophe from "@/assests/icons/double-quotes.png";
 import trainerPhoto from "@/assests/images/trainer.jpg";
 import Image from "next/image";
+import { nunito, rubik } from "@/app/config/fonts";
 
-const rubik = Rubik({
-  weight: ["300", "400", "700", "900"],
-  subsets: ["latin"],
-  preload: true,
-});
-
-const nunito = Nunito({
-  weight: ["300", "400", "700", "900"],
-  subsets: ["latin"],
-  preload: true,
-});
+const reviewData = [
+  {
+    reviewImage: trainerPhoto,
+    reviewerName: "Ryan Carter",
+    reviewText:
+      "Feels like home! Motivating, clean, and full of positive energy!",
+  },
+  {
+    reviewImage: trainerPhoto,
+    reviewerName: "Stephen Martinez",
+    reviewText:
+    "Best fitness experience ever! I’ve seen real results in just a few weeks!",
+  },
+  {
+    reviewImage: trainerPhoto,
+    reviewerName: "Jake Thompson",
+    reviewText:
+      "Amazing gym! Great trainers, top-notch equipment, and an awesome vibe. Love it!",
+  },
+];
 
 const Testimonial = () => {
   return (
@@ -45,12 +54,47 @@ const Testimonial = () => {
                 className="w-full object-cover -rotate-45"
               />
             </div>
-            <p className="text-center py-10 text-2xl uppercase">
-              more section in progress...
-            </p>
           </div>
         </div>
       </div>
+        {/* <<<<<<<<<<<<<<<<<<<<<<< Review section >>>>>>>>>>>>>>>>>>> */}
+        <div className=" lg:max-w-7xl md:max-w-3xl sm: max-w-sm lg:mx-auto md:mx-8 sm: mx-10 ">
+          {reviewData.map(
+            ({ reviewImage, reviewerName, reviewText }, index) => (
+              <div
+                key={index}
+                className="grid lg:grid-cols-2 md:grid-cols-2 sm: grid-cols-1 items-center lg:mx-32"
+              >
+                <div>
+                  <Image
+                    src={reviewImage}
+                    alt="Trainer photo"
+                    className="w-[400px] h-[600px] "
+                  />
+                </div>
+                {/* ---------------------------------------- */}
+                <div className="relative">
+                  <div className=" relative lg:mx-6">
+                  <p className={` ${rubik.className} text-[22px] mb-3 `}>{reviewText}</p>
+                  <h1 className={` ${nunito.className} font-bold text-xl `}>{reviewerName}</h1>
+                  </div>
+                  <div>
+                  <Image
+                    src={apostrophe}
+                    alt="apostrophe icon"
+                    className="w-36 absolute -top-28 -left-24"
+                  />
+                   <Image
+                    src={apostrophe}
+                    alt="apostrophe icon"
+                    className="w-36 absolute -bottom-20 right-2"
+                  />
+                  </div>
+                </div>
+              </div>
+            )
+          )}
+        </div>
     </div>
   );
 };
---
+++ src/app/config/fonts.js
@@ -0,0 +1,18 @@
+
+import { Rubik, Nunito } from "next/font/google";
+
+
+export const rubik = Rubik({
+  weight: ["300", "400", "700", "900"],
+  subsets: ["latin"],
+  preload: true,
+});
+
+export const nunito = Nunito({
+  weight: ["300", "400", "700", "900"],
+  subsets: ["latin"],
+  preload: true,
+});
+
+
+
